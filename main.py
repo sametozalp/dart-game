96fd674b10346bb85d38464d8740ce4e19d1d1a6
sametozalp <sametozalp0056@gmail.com>
2024-05-03 22:40:15 +0300

    added sky

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 from ball import Ball
 from gun import Gun
 import tkinter as tk
+import serial
+import pydirectinput
 from tkinter import messagebox
 
 root = tk.Tk()
@@ -23,11 +25,57 @@ running = True
 pygame.mixer.init()
 sound = pygame.mixer.Sound("gun.mp3")
 
+arduino = serial.Serial('COM8', 115200, timeout=.1)     #serial input from arduino. change COM port to wherever your arduino is connected
+
+pydirectinput.PAUSE = 0
+
+keysDown = {}   #list of currently pressed keys
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+bg = pygame.image.load('sky.png')
+def background_sky():
+    size = pygame.transform.scale(bg, (1920,1080))
+    screen.blit(size, (0,0))
+
+def keyDown(key):               #what to do if key pressed. takes value from handleJoyStickAsArrowKeys
+    keysDown[key] = True        #adds key to KeysDown list
+    pydirectinput.keyDown(key)  #runs pydirectinput using key from (argument)
+    #print('Down: ', key)       #remove '#' from print to test data stream
+
+
+def keyUp(key):                     #what to do if key released. takes value from handleJoyStickAsArrowKeys
+    if key in keysDown:
+        del (keysDown[key])         #remove key from KeysDown
+        pydirectinput.keyUp(key)    #runs pydirectinput using key from (argument)
+        #print('Up: ', key)         #remove '#' from print to test data stream
+
+
+def handleJoyStickAsArrowKeys(x, y, z):      #note that the x and y directions are swapped due to the way I orient my thumbstick
+    if x == 2:          #0 is up on joystick
+        keyDown('up')   #add up key to keyDown (argument)
+        keyUp('down')   #add down key to keyUp (argument), as you can't press up and down together
+    elif x == 0:        #2 is down on joystick
+        keyDown('down')
+        keyUp('up')
+    else:               #1 is neutral on joystick
+        keyUp('up')
+        keyUp('down')
+
+    if y == 2:          #2 is right on joystick
+        keyDown('right')
+        keyUp('left')
+    elif y == 0:        #0 is left on joystick
+        keyDown('left')
+        keyUp('right')
+    else:               #1 is neutral on joystick
+        keyUp('left')
+        keyUp('right')
+        
 def main():
     pygame.init()
     global score
     global running
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     pygame.display.set_caption("Dart Game")
 
     clock = pygame.time.Clock()
@@ -92,7 +140,8 @@ def main():
             
         all_sprites.update()
 
-        screen.fill(WHITE)
+        #screen.fill(WHITE)
+        background_sky()
         all_sprites.draw(screen)
 
         score_text = font.render("Skor: {}".format(score), True, BLACK)
@@ -104,6 +153,16 @@ def main():
 
         pygame.display.flip()
         clock.tick(60)
+        
+        #############################################################3
+        rawdata = arduino.readline()            #read serial data from arduino one line at a time
+        data = str(rawdata.decode('utf-8'))     #decode the raw byte data into UTF-8
+        if data.startswith("S"):                #make sure the read starts in the correct place
+            dx = int(data[1])                   #X direction is second digit in data (data[0] is 'S')
+            dy = int(data[3])                   #Y direction is fourth digit in data
+            JSButton = int(data[5])             #JSButton is sixth digit in data
+            #print(dx, dy, JSButton)            #remove '#' from print to test data stream
+            handleJoyStickAsArrowKeys(dx, dy, JSButton)     #run body of code using dx, dy and JSButton as inputs
     
     root.destroy()
 
